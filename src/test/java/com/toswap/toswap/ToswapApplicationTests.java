6092d963f48141f84daac690204a65e010a1943c
lee0806 <leeseh0806@gmail.com>
2026-05-28 16:28:07 +0900

    init: 프로젝트 생성
    Signed-off-by: lee0806 <leeseh0806@gmail.com>

---
+++ src/test/java/com/toswap/toswap/ToswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toswap.toswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToswapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
